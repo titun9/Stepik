b95674b2b83d99c128db406d1328db4d0b48e681
ButkevichAS <butkevichas@cheops-edu.ru>
2021-05-28 13:40:36 +0300

    Change assert to find element

--- module_3/test_sample.py
+++ module_3/test_sample.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
 
 # Links
 link_login_page = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -17,10 +21,6 @@ welcome_message_locator = ".alertinner.wicon"
 
 
 def test_login_user():
-    # Data
-    welcome_message = "Рады видеть вас снова"
-    name_logout_link = "Logout"
-    name_account_link = "Account"
 
     try:
         # Arrange
@@ -38,15 +38,18 @@ def test_login_user():
         button_login.click()
 
         # Assert
-        welcome_message_text = browser.find_element_by_css_selector(welcome_message_locator).text
-        assert welcome_message == welcome_message_text, \
-            f"Ожидается приветственное сообщение '{welcome_message}', а получено '{welcome_message_text}'"
-        logout_link_text = browser.find_element_by_css_selector(logout_link_locator).text
-        assert name_logout_link == logout_link_text, \
-            f"Ожидается название кнопки выхода из аккаунта '{name_logout_link}', а получено '{logout_link_text}'"
-        account_link_text = browser.find_element_by_css_selector(account_link_locator).text
-        assert name_account_link == account_link_text, \
-            f"Ожидается название кнопки аккаунта '{name_account_link}', а получено '{account_link_text}'"
+        WebDriverWait(browser, 5).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, welcome_message_locator)),
+            message="Не отображается приветственное сообщение"
+        )
+        WebDriverWait(browser, 5).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, logout_link_locator)),
+            message="Не отображается кнопка выхода из аккаунта пользователя"
+        )
+        WebDriverWait(browser, 5).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, account_link_locator)),
+            message="Не отображается кнопка перехода в аккаунт пользователя"
+        )
 
     finally:
         browser.quit()
