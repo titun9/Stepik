da0b770fd9eb11afca7b7ca99841600edd4ceeff
ButkevichAS <butkevichas@cheops-edu.ru>
2021-06-18 12:49:45 +0300

    Fix error of getting current url

--- module_5/pages/login_page.py
+++ module_5/pages/login_page.py
@@ -9,8 +9,8 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        current_link = self.browser.current_url()
-        assert "login" in current_link, "No page name in the link"
+        current_link = self.browser.current_url
+        assert 'login' in current_link, "No page name in the link"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
